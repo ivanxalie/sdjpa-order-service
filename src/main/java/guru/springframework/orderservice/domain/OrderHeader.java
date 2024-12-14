7d70ddd612924ba73e45ec9cc95ba98e831571c6
ivanxalie <ivanxalie@gmail.com>
2024-12-14 22:31:24 +0300

    Migrate to liquibase. Add lombok

--- src/main/java/guru/springframework/orderservice/domain/OrderHeader.java
+++ src/main/java/guru/springframework/orderservice/domain/OrderHeader.java
@@ -1,52 +1,25 @@
 package guru.springframework.orderservice.domain;
 
-import  jakarta.persistence.Entity;
-import  jakarta.persistence.GeneratedValue;
-import  jakarta.persistence.GenerationType;
-import  jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Created by jt on 12/5/21.
  */
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
 public class OrderHeader {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String customer;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getCustomer() {
-        return customer;
-    }
-
-    public void setCustomer(String customer) {
-        this.customer = customer;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        OrderHeader that = (OrderHeader) o;
-
-        if (id != null ? !id.equals(that.id) : that.id != null) return false;
-        return customer != null ? customer.equals(that.customer) : that.customer == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (customer != null ? customer.hashCode() : 0);
-        return result;
-    }
 }
