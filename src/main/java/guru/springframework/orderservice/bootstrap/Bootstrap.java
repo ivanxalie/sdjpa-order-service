3a47996b53e8677e9da63babf9113e8cc653e23f
ivanxalie <ivanxalie@gmail.com>
2024-12-25 20:23:21 +0300

    Add optimistic lock example for customer

--- src/main/java/guru/springframework/orderservice/bootstrap/Bootstrap.java
+++ src/main/java/guru/springframework/orderservice/bootstrap/Bootstrap.java
@@ -2,6 +2,7 @@ package guru.springframework.orderservice.bootstrap;
 
 import guru.springframework.orderservice.domain.Customer;
 import guru.springframework.orderservice.repositories.CustomerRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
@@ -25,7 +26,14 @@ public class Bootstrap implements CommandLineRunner {
         Customer saved = customerRepository.save(customer);
         log.info("Customer version: {}", saved.getVersion());
 
-        customerRepository.deleteById(saved.getId());
+        saved.setName("Testing Version 2");
+        Customer saved2 = customerRepository.save(saved);
+        log.info("Customer version: {}", saved2.getVersion());
 
+        saved.setName("Testing Version 3");
+        Customer saved3 = customerRepository.save(saved2);
+        log.info("Customer version: {}", saved3.getVersion());
+
+        customerRepository.delete(saved3);
     }
 }
